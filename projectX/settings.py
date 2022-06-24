b540fda14ae979d0570c651eb6c9ce6238c40f96
Dhruvil Mehta <68022411+dhruvilmehta@users.noreply.github.com>
2022-06-24 13:29:43 +0530

    Update settings.py

--- projectX/settings.py
+++ projectX/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'vqj%5z_bv_bgvp@51*q)yqny@-hta%n@g#elf2owfavg2ulnkh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','projectxweb1.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','bookecomm.herokuapp.com']
 
 
 # Application definition
