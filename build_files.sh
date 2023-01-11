6d4275ba1e9291f61246d42699492d0851a9ee3a
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2023-01-11 17:27:04 +0530

    Configured

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo " BUILD START"
-python3.9 -m pip install -r requirements.txt
+pip install db-sqlite3
+python3.10.0 -m pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python3.9 manage.py collectstatic --noinput --clear
