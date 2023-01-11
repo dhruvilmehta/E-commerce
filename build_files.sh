4f7d9837f9e69dc565463f6388c79639d6ec1eb8
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2023-01-11 17:35:16 +0530

    Configured

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo " BUILD START"
+python3 -m pip install --user virtualenv
+python3 -m venv env
+source env/bin/activate
 pip install db-sqlite3
 python -m pip install -r requirements.txt
 python manage.py makemigrations
