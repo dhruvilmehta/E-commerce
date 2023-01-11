bd802684e39b061d3a87186d3b7f2748570a8f10
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2023-01-11 18:04:54 +0530

    Configured

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,11 @@
 echo " BUILD START"
 yum install sqlite-devel
 
-./configure
-make && make altinstall
+./configure make && make altinstall
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate
-
+pyenv install 3.10.0
 pip install db-sqlite3
 python -m pip install -r requirements.txt
 python manage.py makemigrations
