05151732fd9cb3ebaa6b1f9abef9bf8a0ef7c772
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2023-01-11 17:51:35 +0530

    Configured

--- build_files.sh
+++ build_files.sh
@@ -2,7 +2,8 @@ echo " BUILD START"
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate
-pip install sqlite-devel
+pip install libsqlite3-dev
+./configure --enable-loadable-sqlite-extensions && make && sudo make install
 pip install db-sqlite3
 python -m pip install -r requirements.txt
 python manage.py makemigrations
