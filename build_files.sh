69a64209d2edab1419f266e6fce6476632bd8485
Dhruvil Mehta <dhruvilmehta2606@gmail.com>
2023-01-11 17:22:25 +0530

    Configured

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 echo " BUILD START"
 python3.9 -m pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END" 
